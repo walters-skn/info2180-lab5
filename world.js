a509bd6e9c5cd70ee2c5b85ca88c84fdf2f4c7ef
walters-skn <mickywalters72@gmail.com>
2021-11-04 08:13:05 -0400

    Completed Lab

--- world.js
+++ world.js
@@ -1,6 +1,7 @@
 window.onload=function(){
     let search = document.getElementById("lookup");
-    var url = 'http://localhost:8888/info2180-lab5/world.php?country=';
+    let searchbtn2 = document.getElementById("lookup-cty")
+    // var url = 'http://localhost:8888/info2180-lab5/world.php?country=';
 
     search.addEventListener('click', function(e){
 
@@ -23,7 +24,31 @@ window.onload=function(){
             }
         }
         console.log(s);
-        req.open('GET', url+s, true);
+        req.open('GET', 'http://localhost:8888/info2180-lab5/world.php?country='+s, true);
+        req.send();
+    });
+
+    searchbtn2.addEventListener('click', function(e){
+        console.log('button clicked')
+        console.log('cities returned')
+
+        e.preventDefault();
+        var s = document.getElementById("country").value;
+        var req = new XMLHttpRequest();
+
+        req.onreadystatechange = function() {
+            if (req.readyState == XMLHttpRequest.DONE) {
+                if (req.status == 200){	
+                    var r = req.responseText;
+                    var result = document.getElementById('result');
+                    result.innerHTML = r;
+                }else{
+					alert("There was a problem with the request.");
+                }
+            }
+        }
+        console.log(s);
+        req.open('GET', 'http://localhost:8888/info2180-lab5/world.php?country='+s+'&context=cities', true);
         req.send();
     });
 } 
--- world.php
+++ world.php
@@ -12,10 +12,18 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country = filter_input(INPUT_GET,'country',FILTER_SANITIZE_STRING);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$context = filter_input(INPUT_GET,'context',FILTER_SANITIZE_STRING);
 
- $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
- ?>
+if ($context == "cities") {
+	$stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'");
+}
+else 
+{
+	$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
 
 <table>
 <?php if ($context == 'cities'):?>
